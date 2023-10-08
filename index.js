f42c3854b97d8be379996dd088387beaae6cc21b
Edgar <eddyade98@gmail.com>
2023-10-08 16:24:46 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// function expression named divide that divides the two parameters placed in the function
+function divide () {
+    return 2000 / 100;
+}
+
+//arrow function named square that takes one parameter and multiplies by itself
+const square = x => x * x;
+x(2);
+
+//arrow function named add that takes tow parameters and adds them
+const add = (a, b) => a + b;
+add(3,4);
+
+
